284bcd03f25a120cb55e5e66895e579f405a3db8
kamilego <80158455+kamilego@users.noreply.github.com>
2021-04-20 11:10:17 +0200

    Update pdf_merge.py
    now you can merge single pdfs or select certain path - merge all pdfs there

--- pdf_merge.py
+++ pdf_merge.py
@@ -1,8 +1,29 @@
 import PyPDF2 
 import warnings
+import os
 
 
-def merge_pdf(path):
+# execution merge pdfs according to path
+def multi_merge(path):
+    warnings.filterwarnings("ignore")
+    pdfWriter = PyPDF2.PdfFileWriter()
+    pdf_new = open("%s\_SIR1.pdf" % path, 'wb')
+    merge_all(path, pdfWriter, pdf_new)
+    pdf_new.close()
+
+
+# execution merge single pdf
+def merge_single(path):
+    pdfWriter = PyPDF2.PdfFileWriter()
+    pdf_new = open("%s\_SIR1.pdf" % path, 'wb')
+    merge_pdf("D:\kamil\KAR4301B\pdf\KAR4301B_REW.01_210420_PS01.pdf", pdfWriter, pdf_new)
+    merge_pdf("D:\kamil\KAR4301B\pdf\KAR4301B_REW.01_210420_T01.pdf", pdfWriter, pdf_new)
+    merge_pdf("D:\kamil\KAR4301B\pdf\KAR4301B_REW.02_210420_T02_TAB.pdf", pdfWriter, pdf_new)
+    pdf_new.close()
+
+
+# to merge single pdfs
+def merge_pdf(path, pdfWriter, pdf_new):
     pdf_open = open(path, 'rb')
     pdf_reader = PyPDF2.PdfFileReader(pdf_open, strict=False)
     for pageNum in range(pdf_reader.numPages):
@@ -10,12 +31,17 @@ def merge_pdf(path):
         pdfWriter.addPage(pageObj)
     pdfWriter.write(pdf_new)
     pdf_open.close()
-    
+
+
+# create list of pdfs in path
+def merge_all(path, pdfWriter, pdf_new):
+    pdf_all = os.listdir(path)
+    for elem in pdf_all:
+        merge_pdf("%s\%s" % (path, elem), pdfWriter, pdf_new)
+
 
 warnings.filterwarnings("ignore")
-pdfWriter = PyPDF2.PdfFileWriter()
-pdf_new = open(r"D:\kamil\BYD1118A\pdf\_SIR2.pdf", 'wb')
-merge_pdf(r"D:\kamil\BYD1118A\pdf\BYD1118A_REW.01_210416_T01.pdf")
-merge_pdf(r"D:\kamil\BYD1118A\pdf\BYD1118A_REW.01_210416_T02_TAB.pdf")
-merge_pdf(r"D:\kamil\BYD1118A\pdf\BYD1118A_REW.01_210416_T03_TAB.pdf")
-pdf_new.close()
+
+merge_single("D:\kamil\KAR4301B\pdf")
+
+multi_merge("D:\kamil\KAR4301B\pdf")
