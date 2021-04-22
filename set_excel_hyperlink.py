f2c13dde7b5e78e32417da9a7252af25fcbb0ca1
Kamil <80158455+kamilego@users.noreply.github.com>
2021-04-22 11:51:13 +0200

    Update set_excel_hyperlink.py
    changes to excel cells and scripts to be available to AutoCAD LT

--- set_excel_hyperlink.py
+++ set_excel_hyperlink.py
@@ -6,6 +6,7 @@ import shutil
 import datetime
 
 
+# replaces second appearing word to destiny word 
 def replace_second(string, a, b): 
    return string.replace(a, b, 2).replace(b, a, 1) 
 
@@ -40,14 +41,14 @@ def first_steps(base_path, new_project, dwg_dir, xlsx_path, dic_w_replace):
         # creating .scr file - execution script file
         s = open("%(x)s\%(y)s\%(y)s.scr" % {"x": base_path, "y": new_project}, "w")
         with s as filehandle:
-            filehandle.writelines('(command "-PUBLISH" "%(x)s/%(y)s/%(y)s.dsd")\n' % {"x": base_path.replace("\\","/"), "y": new_project})
+            filehandle.writelines('-PUBLISH\n%(x)s/%(y)s/%(y)s.dsd\n' % {"x": base_path.replace("\\","/"), "y": new_project})
 
         # opening script file to change tabs layout dwg names
         k = open(r"D:\kamil\_scripts\templates\uklady.scr")
         l = k.readlines()
-        for i in range(len(l)):
+        for i in range(3,24,4):
             for key, value in dic_w_replace.items():
-                l[i] = replace_second(l[i], key, value)
+                l[i] = l[i].replace(key, value)
 
         # creating .scr file - changing tabs names
         g = open("%(x)s\%(y)s\%(y)suklad.scr" % {"x": base_path, "y": new_project}, "w")
@@ -69,15 +70,15 @@ def edit_excel_histry(list_values):
         first_set = set()
         second_set = set(list_values)
         for num in range(length):
-            first_set.add(ws["A"+str(num+5)].value)
+            first_set.add(ws["A"+str(num+1)].value)
         if len(first_set - second_set) == 0:
             print("List of folders in excel and path are the same\nNothing has been added.")
             break
-        row_num = list_values.index(list(second_set - first_set)[0])+5
+        row_num = list_values.index(list(second_set - first_set)[0])+1
         ws.insert_rows(row_num)
         second_set = list(second_set)
         second_set.sort()
-        for num, elem in enumerate(second_set, start=5):
+        for num, elem in enumerate(second_set, start=1):
             wb.active['A'+str(num)].hyperlink = "D:/kamil/"+elem
             wb.active['A'+str(num)].value = elem
             wb.active['A'+str(num)].style = "Hyperlink"
@@ -97,6 +98,7 @@ def list_of_folders():
 
 
 # function that creates new excel with actual hyperlink to folders
+# if xlsx file exists previous is copied to _old folder
 # use it only in critical case
 def create_excel_histry(lista):
     now = str(datetime.datetime.now())[:19]
@@ -109,7 +111,7 @@ def create_excel_histry(lista):
         wb = Workbook()
         ws = wb.active
         ws.column_dimensions["A"].width = 12
-        for num, elem in enumerate(lista, start=5):
+        for num, elem in enumerate(lista, start=2):
             wb.active['A'+str(num)].hyperlink = "D:/kamil/"+elem
             wb.active['A'+str(num)].value = elem
             wb.active['A'+str(num)].style = "Hyperlink"
