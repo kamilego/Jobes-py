b661806a2c9d716b95ae74e45d135e9b6031e334
Kamil <80158455+kamilego@users.noreply.github.com>
2021-06-15 08:46:49 +0200

    Update max_forces.py

--- forces/max_forces.py
+++ forces/max_forces.py
@@ -1,11 +1,13 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 from openpyxl.styles import PatternFill, Border, Side
 from math import sqrt
 import time
 import codecs
 import csv
 
+
 begin_time = time.time()
+
 
 def check_value(row):
     z = max(l[3] for l in row)
@@ -38,8 +40,8 @@ def cells_range(rowik, a,b):
 
 
 def sumq(row, value):
-    ws.cell(row, value+1).value = cells_range(row, value+2, value+14)
-    cell_properties(ws.cell(row, value+1),"00FF6600")
+    ws.cell(row, value).value = cells_range(row, value+1, value+11)
+    cell_properties(ws.cell(row, value),"00FF6600")
 
 
 def minus_value_check(row, col):
@@ -58,7 +60,7 @@ def list_check(lis):
 
 def max_force(row,col, a):
     val_list = []
-    for num in range(3,29*4,13):
+    for num in range(3,99,11):
         val_list.append(minus_value_check(row-1,num))
     val_list = sorted(val_list)
     val_list = list_check(val_list)
@@ -70,8 +72,14 @@ def max_force(row,col, a):
     cell_properties(ws.cell(row, col),"00008000")
 
 
-path = r"D:\kamil\_scripts\pythony\zygry\krawezniki\krawezniki.csv"
+def cell_values(num1, step, row):
+    for num in range(num1, num1+step):
+        ws.cell(row+2, num).value = ws.cell(row+1, num1-1).value - ws.cell(row+1, num).value
+
+
+path = r"D:\kamil\_scripts\pythony\siedlce\krawezniki\krawezniki.csv"
 path2 = path[:-3] + "xlsx"
+path3 = path[:-4] + "_temp.xlsx"
 
 with codecs.open(path,"rb","utf-16") as f:
     a = csv.reader(f,delimiter='\t')
@@ -90,27 +98,23 @@ for num in range(len(a)):
     a[num][3] = float(a[num][3].replace(",","."))
 
 a = sorted(a, key=lambda x: x[2])
-
-
-all_list = [a[num:num+10] for num in range(0,len(a),10)]
-
-
-
+all_list = [a[num:num+25] for num in range(0,len(a),25)]
 diction ={}
 
 for row in all_list:
     diction[check_value(row)[0]] = check_value(row)[1]
 
 aa = {}
+rod_num = int(list(diction.keys())[-1].split("_")[1])
 
-for elem in range(458):
-	for num in range(0,len(diction),458):
+for elem in range(rod_num):
+	for num in range(0,len(diction),rod_num):
 		aa[list(diction.keys())[elem+num]] = diction[list(diction.keys())[elem+num]]
 
 prety = sorted(set([int(elem.split("_")[1]) for elem in list(aa.keys())]))
 kombinacje = sorted(set([int(elem.split("_")[0]) for elem in list(aa.keys())]))
 
-wb = Workbook()
+wb = load_workbook(path3)
 ws = wb.active
 
 row1 = 3
@@ -131,61 +135,17 @@ for to in range(3,len(prety)*3+1,3):
         cell_properties(ws.cell(to, ro),"00FFCC99")
         ami+=1
 
-
 for to in range(2,len(prety)*3+2,3):
-    for num in range(5,17):
-        ws.cell(to+2, num).value = ws.cell(to+1, 4).value - ws.cell(to+1, num).value
-    for num in range(18,30):
-        ws.cell(to+2, num).value = ws.cell(to+1, 17).value - ws.cell(to+1, num).value
-    for num in range(31,43):
-        ws.cell(to+2, num).value = ws.cell(to+1, 30).value - ws.cell(to+1, num).value
-    for num in range(44,56):
-        ws.cell(to+2, num).value = ws.cell(to+1, 43).value - ws.cell(to+1, num).value
-    for num in range(57,69):
-        ws.cell(to+2, num).value = ws.cell(to+1, 56).value - ws.cell(to+1, num).value
-    for num in range(70,82):
-        ws.cell(to+2, num).value = ws.cell(to+1, 69).value - ws.cell(to+1, num).value
-    for num in range(83,95):
-        ws.cell(to+2, num).value = ws.cell(to+1, 82).value - ws.cell(to+1, num).value
-    for num in range(96,108):
-        ws.cell(to+2, num).value = ws.cell(to+1, 95).value - ws.cell(to+1, num).value
-    for num in range(109,121):
-        ws.cell(to+2, num).value = ws.cell(to+1, 108).value - ws.cell(to+1, num).value
-    for num in range(3,120,13):
+    for num in range(5,94,11):
+        cell_values(num, 10, to)
+    for num in range(4,102,11):
         sumq(to+2,num)
     max_force(to+2, 2, 0)
     max_force(to+2, 1, 1)
-
-
-ws2 = wb.create_sheet("Segmenty")
-ws2.cell(2,5).value = "Plusy"
-ws2.cell(2,6).value = "Minusy"
-ws2.cell(2,7).value = "Segment"
-ws2.cell(2,8).value = "Wartość"
-ws2.cell(2,9).value = "Wytężenie"
-
-for num in range(3,49):
-    ws2.cell(num,7).value = num-2
-
-a12 = [num for num in range(38,1064,27)]
-"=MAX(Sheet!B1:B46)"
-for num, val in enumerate(a12,start=4):
-    ws2.cell(num,5).value = f"=MAX(Sheet!A{val}:A{val+27})"
-    ws2.cell(num,6).value = f"=MIN(Sheet!B{val}:B{val+27})"
-
-a22 = [num for num in range(1064,1352+16*3,16*3)]
-for num, val in enumerate(a22,start=42):
-    ws2.cell(num,5).value = f"=MAX(Sheet!A{val}:A{val+48})"
-    ws2.cell(num,6).value = f"=MIN(Sheet!B{val}:B{val+48})"   
-
-ws2.cell(3,5).value = f"=MAX(Sheet!A3:A37)"
-ws2.cell(3,6).value = f"=MIN(Sheet!B3:B37)"
-
 
 
 wb.save(path2)
 
 
 print(round(time.time() - begin_time,2))
-
 
