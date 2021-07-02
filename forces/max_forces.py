3db4f1c974f0636d02afb254c9c1ffac50e3e877
Kamil <80158455+kamilego@users.noreply.github.com>
2021-07-02 13:23:39 +0200

    Update max_forces.py
    to use script have to change only three parameters: path, division, komb_d

--- forces/max_forces.py
+++ forces/max_forces.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 from openpyxl.styles import PatternFill, Border, Side
 from math import sqrt
 import time
@@ -21,18 +21,17 @@ def check_value(row):
 def change(elem):
     if elem.count("/") == 3:
         pos = elem.index('/', elem.index('/') + 1)
-        elem = elem[:pos] + 'KAM' + elem[pos + 1:]
+        elem = elem[:pos] + 'KAM' + elem[pos+1:]
     return elem
 
 
 def cell_properties(cell,color):
-    y = cell
-    y.fill = PatternFill(fgColor=color, fill_type = "solid")
+    cell.fill = PatternFill(fgColor=color, fill_type="solid")
     thin = Side(border_style="thin", color="000000")
-    y.border = Border(top=thin, left=thin, right=thin, bottom=thin)
+    cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
 
-def cells_range(rowik, a,b):
+def cells_range(rowik,a,b):
     sum_val = 0
     for num in range(a,b):
         sum_val += (ws.cell(rowik, num).value)**2
@@ -40,7 +39,7 @@ def cells_range(rowik, a,b):
 
 
 def sumq(row, value):
-    ws.cell(row, value).value = cells_range(row, value+1, value+11)
+    ws.cell(row, value).value = cells_range(row, value+1, value+komb_d)
     cell_properties(ws.cell(row, value),"00FF6600")
 
 
@@ -58,9 +57,9 @@ def list_check(lis):
         return lis
 
 
-def max_force(row,col, a):
+def max_force(row,col,a):
     val_list = []
-    for num in range(3,99,11):
+    for num in range(3,len(kombinacje),komb_d):
         val_list.append(minus_value_check(row-1,num))
     val_list = sorted(val_list)
     val_list = list_check(val_list)
@@ -77,7 +76,15 @@ def cell_values(num1, step, row):
         ws.cell(row+2, num).value = ws.cell(row+1, num1-1).value - ws.cell(row+1, num).value
 
 
+def cell_value_fill(row, num, name, color):
+    ws.cell(row, num).value = name
+    cell_properties(ws.cell(row, num), color)
+
+
 path = r"D:\kamil\_scripts\pythony\siedlce\krawezniki\krawezniki.csv"
+division = 25
+komb_d = 11
+
 path2 = path[:-3] + "xlsx"
 path3 = path[:-4] + "_temp.xlsx"
 
@@ -98,14 +105,14 @@ for num in range(len(a)):
     a[num][3] = float(a[num][3].replace(",","."))
 
 a = sorted(a, key=lambda x: x[2])
-all_list = [a[num:num+25] for num in range(0,len(a),25)]
-diction ={}
+all_list = [a[num:num+division] for num in range(0,len(a),division)]
+diction = {}
 
 for row in all_list:
     diction[check_value(row)[0]] = check_value(row)[1]
 
 aa = {}
-rod_num = int(list(diction.keys())[-1].split("_")[1])
+rod_num = int(list(diction.keys())[-1].split("_")[1])-int(list(diction.keys())[0].split("_")[1]) + 1
 
 for elem in range(rod_num):
 	for num in range(0,len(diction),rod_num):
@@ -120,25 +127,23 @@ ws = wb.active
 row1 = 3
 num1 = 3
 for name in prety:
-    ws.cell(row1, num1).value = name
-    cell_properties(ws.cell(row1, num1),"00C0C0C0")
+    cell_value_fill(row1, num1, name, "00C0C0C0")
     row1 += 3
 
 for row in range(2,len(prety)*3,3):
     for num, name in enumerate(kombinacje,start=4):
-        ws.cell(row, num).value = name
-        cell_properties(ws.cell(row, num),"00666699")
+        cell_value_fill(row, num, name, "00666699")
+
 ami = 0
 for to in range(3,len(prety)*3+1,3):
     for ro in range(4,len(kombinacje)+4):
-        ws.cell(to, ro).value = list(aa.values())[ami]
-        cell_properties(ws.cell(to, ro),"00FFCC99")
+        cell_value_fill(to, ro, list(aa.values())[ami], "00FFCC99")
         ami+=1
 
 for to in range(2,len(prety)*3+2,3):
-    for num in range(5,94,11):
-        cell_values(num, 10, to)
-    for num in range(4,102,11):
+    for num in range(5,len(kombinacje)-1,komb_d):
+        cell_values(num, komb_d-1, to)
+    for num in range(4,len(kombinacje)+3,komb_d):
         sumq(to+2,num)
     max_force(to+2, 2, 0)
     max_force(to+2, 1, 1)
