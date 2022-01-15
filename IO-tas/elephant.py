f8f5f55e431f7df26b755f674494f72d8b1a79eb
Kamil <80158455+kamilego@users.noreply.github.com>
2022-01-15 22:24:23 +0100

    input in cmd as file path, all done

--- IO-tas/elephant.py
+++ IO-tas/elephant.py
@@ -1,47 +1,35 @@
-def main():
-    path = r"D:\Programy\z.programowanie\elephant\zadanie_B\slo2.in"
-    with open(path) as f:
-        c = f.read()
-    c = c.split("\n")[:-1]
+def load_input() -> list:
+    values = [input() for _ in range(4)]
     for i in range(1, 4):
-        c[i] = list(map(int, c[i].split()))
-    ele_weight = {k: v for k, v in enumerate(c[1], start=1)}
-    elephants = []
-    for i in range(int(c[0])):
-        if not c[2][i] == c[-1][i]:
-            elephants.append((c[2][i], c[-1][i]))
-        else:
-            del ele_weight[c[2][i]]
-    mass = 0
-    for elem in elephants:
-        if elem[::-1] in elephants:
-            mass += ele_weight[elem[0]]+ele_weight[elem[1]]
-            for i in [elem, elem[::-1]]:
-                elephants.remove(i)
-    ai = 1
+        values[i] = list(map(int, values[i].split()))
+    return values
+
+
+def calc_sum_mass(values) -> int:
+    eleph_weight = {eleph_num: mass for eleph_num, mass in enumerate(values[1], start=1)}
+    elephants = [(values[2][i], values[3][i]) for i in range(int(values[0]))]
+    step = 1
     sort_eleph = {}
     while len(elephants) > 0:
-        sort_eleph[ai] = [elephants.pop()[0]]
-        for elem in sort_eleph[ai]:
-            for i in elephants:
-                if elem in i:
-                    sort_eleph[ai].append(elephants.pop(elephants.index(i))[0])
-        ai+=1
-    min_global = ele_weight[min(ele_weight, key=ele_weight.get)]
-    # print(min_global)
+        sort_eleph[step] = [elephants.pop()]
+        for elem in sort_eleph[step]:
+            for elephant in elephants:
+                if elem[0] in elephant:
+                    sort_eleph[step].append(elephants.pop(elephants.index(elephant)))
+        step += 1
+    min_global = eleph_weight[min(eleph_weight, key=eleph_weight.get)]
+    total_mass = 0
     for key in sort_eleph:
-        sort_eleph[key] = sorted(list(map(lambda x: ele_weight[x], sort_eleph[key])))
-        print(f"{len(sort_eleph[key])}{-1}*{min_global}")
-        mass += ((len(sort_eleph[key])-1)*min_global)
-        print(f"{2}*({sort_eleph[key][0]}+{min_global})")
-        mass += 2*sort_eleph[key][0]
-        for elem in sort_eleph[key][1:]:
-            # print(elem)
-            mass += elem
-    print()
-    print(mass)
-    with open(path.replace("in", "out")) as h:
-        l = h.read()
-        print(l.strip())
+        sort_eleph[key] = list(map(lambda x: eleph_weight[x[0]], sort_eleph[key]))
+        a = sum(sort_eleph[key])+((len(sort_eleph[key])-2)*min(sort_eleph[key]))
+        b = sum(sort_eleph[key])+min(sort_eleph[key])+((len(sort_eleph[key])+1)*min_global)
+        total_mass += min(a,b)
+    return total_mass
+
+
+def main():
+    a = load_input()
+    result = calc_sum_mass(a)
+    print(result)
 if __name__ == '__main__':
     main()
