b2c43b9b2c922fe3e656d4b46c1ff583eb241878
Kamil <80158455+kamilego@users.noreply.github.com>
2022-01-16 16:59:59 +0100

    renamed some variables

--- IO-tas/elephant.py
+++ IO-tas/elephant.py
@@ -7,30 +7,29 @@ def load_input() -> list:
 
 def calc_sum_mass(values) -> int:
     eleph_weight = {eleph_num: mass for eleph_num, mass in enumerate(values[1], start=1)}
-    eleph_position = [(values[2][i], values[3][i]) for i in range(int(values[0]))]
+    eleph_position = [(pos, dest_pos) for pos, dest_pos in zip(values[2], values[3])]
     cycle = 1
     eleph_cycle = {}
     while len(eleph_position) > 0:
         eleph_cycle[cycle] = [eleph_position.pop()[0]]
-        for elem in eleph_cycle[cycle]:
+        for position in eleph_cycle[cycle]:
             for elephant in eleph_position:
-                if elem in elephant:
+                if position in elephant:
                     eleph_cycle[cycle].append(eleph_position.pop(eleph_position.index(elephant))[0])
         eleph_cycle[cycle] = list(map(lambda x: eleph_weight[x], eleph_cycle[cycle]))
         cycle += 1
-    min_global = eleph_weight[min(eleph_weight, key=eleph_weight.get)]
+    min_global_weight = eleph_weight[min(eleph_weight, key=eleph_weight.get)]
     total_mass = 0
-    for key in eleph_cycle:
-        a = sum(eleph_cycle[key])+((len(eleph_cycle[key])-2)*min(eleph_cycle[key]))
-        b = sum(eleph_cycle[key])+min(eleph_cycle[key])+((len(eleph_cycle[key])+1)*min_global)
-        total_mass += min(a,b)
+    for cycle in eleph_cycle:
+        a = sum(eleph_cycle[cycle])+((len(eleph_cycle[cycle])-2)*min(eleph_cycle[cycle]))
+        b = sum(eleph_cycle[cycle])+min(eleph_cycle[cycle])+((len(eleph_cycle[cycle])+1)*min_global_weight)
+        total_mass += min(a, b)
     return total_mass
 
 
 def main():
-    a = load_input()
-    result = calc_sum_mass(a)
+    data = load_input()
+    result = calc_sum_mass(data)
     print(result)
 if __name__ == '__main__':
     main()
-    
