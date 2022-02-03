812bb8a7f0b8a6554c2e47dd048acd1180a1a554
Kamil <80158455+kamilego@users.noreply.github.com>
2022-02-03 23:32:07 +0100

    added dictionary with petrol prices

--- Web-scraping/petrol_scraping.py
+++ Web-scraping/petrol_scraping.py
@@ -1,13 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = r"https://moto.trojmiasto.pl/paliwa/"
-website = requests.get(link)
-soup = BeautifulSoup(website.text, "html.parser")
-table = soup.find(class_="prices-table")
-body = table.select("tr")[1:6]
-for elem in body:
-    petrol_station = elem.find("h3", {"class": "prices-table__name"})
-    price = elem.find("span", {"class": "prices-table__price"})
-    print(petrol_station.text.strip())
-    print(price.text.strip()[:4]+"zł")
+
+def scrap_site():
+    link = r"https://moto.trojmiasto.pl/paliwa/"
+    website = requests.get(link)
+    soup = BeautifulSoup(website.text, "html.parser")
+    table = soup.find(class_="prices-table")
+    body = table.select("tr")[1:11]
+    return body
+
+def create_petrol_dict(body):        
+    petrol_dict = {}
+    for elem in body:
+        petrol_station = elem.find("h3", {"class": "prices-table__name"})
+        price = elem.find("span", {"class": "prices-table__price"})
+        petrol_station =  petrol_station.text.strip()
+        price =  float(price.text.strip()[:4])
+        if not petrol_station in petrol_dict:
+            petrol_dict[petrol_station] = [price]
+        else:
+            petrol_dict[petrol_station].append(price)
+    return petrol_dict
+
+def main():
+    scrapped_element = scrap_site()
+    petrol_dict = create_petrol_dict(scrapped_element)
+    for key, value in petrol_dict.items():
+        print(f"{key}: {min(value)}zł")
+if __name__ == '__main__':
+    main()
