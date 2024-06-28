ebacddfa596c5eae6c897383a51afcb9d65d69a2
kamilego <kam.legowicz@gmail.com>
2024-06-28 21:41:40 +0200

    adjust ms_dp_exam

--- bash_scripts/ms_dp_exam.sh
+++ bash_scripts/ms_dp_exam.sh
@@ -1,18 +1,15 @@
-# to run script go with:
-# bash search.sh "Fiscal"
-word="$1"
-echo "searching for ${word}"
-echo
-cd C:\VSbranch
+# bash exam.sh 1 1 10
+# topic 1 limit of questions 114
 
-for dir in *dbt-*; do
-    if [ -d "$dir" ]; then
-        cd "$dir"
-	echo "$dir"
-        git checkout develop > /dev/null 2>&1
-	git pull > /dev/null 2>&1
-	grep -i -r -o $word . | uniq -c # ignoring string case, recurence searching, match only, count unique values
-	echo
-        cd ..
-    fi
-done
+topic=$1
+for ((num=$2; num<=$3; num++))
+do
+	curl -s "https://www.google.com/search?q=examtopics+dp+203+topic+${topic}+question+${num}" -o temp.txt
+	exam_url=$(grep -o "https://www.examtopics.com/discussions/microsoft/view/.*-exam-dp-203-topic-${topic}-question-${num}-discussion/" temp.txt)
+	if [ -n "$exam_url" ]; then
+        	start chrome "$exam_url"
+	else
+        	echo "Could not find topic: ${topic} question: ${num}"
+	fi
+	rm temp.txt
+done 
