15d2eb6ee63ca2ef727de62671ba1c60724bfdd2
kamilego <kam.legowicz@gmail.com>
2024-07-13 22:03:58 +0200

    aligned script and exam urls

--- bash_scripts/open_dp_exam.sh
+++ bash_scripts/open_dp_exam.sh
@@ -1,5 +1,8 @@
 # bash ms_dp_exam.sh 1 1 10
-# topic 1 limit of questions 114
+# topic 1 limit of questions 114 - missing 13,66,105
+# topic 2 limit of questions 133 - missing 11
+# topic 3 limit of questions 39  - missing 8
+# topic 4 limit of questions 67  - no missings
 
 topic=$1
 for ((num=$2; num<=$3; num++))
